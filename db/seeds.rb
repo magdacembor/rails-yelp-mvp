95aa84e7cfb5e69862df2b24837d37d0b466145f
Magda Cembor <magda.cembor@gmail.com>
2020-02-19 14:27:50 +0000

    full restaurant app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'destroying all restaurants..'
+puts 'creating 5 new restaurants..'
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+  )
+end
+
+puts 'done!'
